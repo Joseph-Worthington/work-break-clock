d59fb288a39d1e76cd9089107d86f2a4974f571d
Joe Worthington <joeyworthington@hotmail.co.uk>
2024-09-10 08:29:36 +0100

    Create basic setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div id="break-label">Break Length</div>
+      <div id="session-label">Session Length</div>
+      <div id="timer-label">Session</div>
+      <div id="time-left">25:00</div>
+      <div id="start_stop">Start/Stop</div>
+      <div id="reset">Reset</div>
+      <div id="break-decrement">Break Decrement</div>
+      <div id="break-increment">Break Increment</div>
+      <div id="session-decrement">Session Decrement</div>
+      <div id="session-increment">Session Increment</div>
+      <div id="break-length">5</div>
+      <div id="session-length">25</div>
     </div>
   );
 }
