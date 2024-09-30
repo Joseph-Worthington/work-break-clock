a6ed18921f24a1b91968cdff25dcfe7a9330f2f6
fishtankjoe <j.worthington@fishtankagency.com>
2024-09-30 18:08:15 +0100

    Code tidy, first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import BreakClock from './components/BreakClock';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [breakLength, setBreakLength] = React.useState(5);
+  const [sessionLength, setSessionLength] = React.useState(25);
+  
+
+
   return (
     <div className="App">
       <div id="break-label">Break Length</div>
       <div id="session-label">Session Length</div>
       <div id="timer-label">Session</div>
-      <div id="time-left">25:00</div>
+      <BreakClock sessionTime={sessionLength} breakTime={breakLength} />
       <div id="start_stop">Start/Stop</div>
       <div id="reset">Reset</div>
       <div id="break-decrement">Break Decrement</div>
+      
       <div id="break-increment">Break Increment</div>
       <div id="session-decrement">Session Decrement</div>
       <div id="session-increment">Session Increment</div>
---
+++ src/components/BreakClock.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface BreakClockProps {
+  breakTime: number;
+  sessionTime: number;
+}
+
+
+const BreakClock: React.FC<BreakClockProps> = ({ breakTime, sessionTime }) => {
+  return (
+    <div>
+      <div id="break-label">Break Length</div>
+      <div id="session-label">Session Length</div>
+      <div id="timer-label">Session</div>
+      <div id="break-length">{breakTime}</div>
+      <div id="session-length">{sessionTime}</div>
+    </div>
+  );
+};
+
+export default BreakClock;
